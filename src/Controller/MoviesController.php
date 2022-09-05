fffc406ff0516e6962867a818c8fde15bb36973d
Nika <nkupatadze792@gmail.com>
2022-09-05 02:34:57 -0700

    Added dependieces, database configuration, templates and configured index&show pages for movies

---
+++ src/Controller/MoviesController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\MovieRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MoviesController extends AbstractController
+{
+    private MovieRepository $movieRepository;
+    public function __construct(MovieRepository $movieRepository)
+    {
+        $this->movieRepository = $movieRepository;
+    }
+
+    #[Route('/movies', name: 'movies', methods: ['GET'])]
+    public function index(): Response
+    {
+        $movies = $this->movieRepository->findAll();
+
+        return $this->render('movies/index.html.twig', [
+            'movies' => $movies
+        ]);
+    }
+
+    #[Route('/movies/{id}', name: 'movie', methods: ['GET'])]
+    public function show($id): Response
+    {
+        $movie = $this->movieRepository->find($id);
+
+        return $this->render('movies/show.html.twig', [
+            'movie' => $movie
+        ]);
+    }
+
+}
